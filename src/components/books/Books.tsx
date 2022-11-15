fe35c0b19675b179f4f8cc64edba89a212b2b934
Michael Chilaka <obichilaka@gmail.com>
2022-11-15 17:35:49 +0100

    updates on Books.tsx, App.scss, index.scss, styles.scss

--- src/components/books/Books.tsx
+++ src/components/books/Books.tsx
@@ -1,6 +1,23 @@
+import "../../assets/scss/styles.scss";
+
 const Books = () => {
   return (
-    <div>Books</div>
+    <div>
+      {/* <!-- Search Area --> */}
+      <div className="search-area">
+        <h1 className="empty-state">Search for your interesting books</h1>
+        <br />
+        <form id="form">
+          <div className="input-group">
+            <input type="text" id="search" placeholder="Search for books..." />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+      </div>
+      <table>
+
+      </table>
+    </div>
   )
 }
 
